de6239f5020a40b9cddabe69c36f5e121c2d297b
hunte <besscomdark@gmail.com>
2023-10-27 22:40:57 +0800

    添加預置物

---
+++ Assets/Scripts/Body/BodyPart.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BodyPart : MonoBehaviour
+{
+    static int bodyID = 0;
+    [HideInInspector] public int bodyPartID;
+    [SerializeField] private GameObject[] bodyPart;
+    [HideInInspector] public Rigidbody bodyPartRigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
